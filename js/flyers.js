2b88b65617964d693b588bc037ebdf44857bfe8a
stephandacosta <stephan.dacosta.tech@gmail.com>
2014-05-07 22:35:44 -0700

    added jquery vmouse to handle mobile tap and desktop clicks

--- js/flyers.js
+++ js/flyers.js
@@ -51,7 +51,7 @@ var Flyers = {
 
   // draw the items as desired
   drawItem: function(network, item){
-
+    console.log(item);
   },
 
 
@@ -111,23 +111,24 @@ var Flyers = {
 
   setNavigation: function($element){
 
-    windy = $element.windy(),
-    allownavnext = false,
+    var windy = $element.windy();
+    var allownavnext = false;
     allownavprev = false;
 
-    $( '#nav-prev' ).on( 'mousedown', function( event ) {
+
+    $( '#nav-prev' ).on( 'vmousedown', function( event ) {
       allownavprev = true;
       navprev();
     })
-    .on( 'mouseup mouseleave', function( event ) {
+    .on( 'vmouseup vmouseleave', function( event ) {
       allownavprev = false;
     });
 
-    $( '#nav-next' ).on( 'mousedown', function( event ) {
+    $( '#nav-next' ).on( 'vmousedown', function( event ) {
       allownavnext = true;
       navnext();
     })
-    .on( 'mouseup mouseleave', function( event ) {
+    .on( 'vmouseup vmouseleave', function( event ) {
       allownavnext = false;
     });
 
